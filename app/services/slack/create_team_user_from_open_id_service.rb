43df19f67414849ab6d75c0e47efbbc38da688a2
beeleethebee <hugo.vast@gmail.com>
2023-08-27 11:10:55 +0200

    :recycle:  create_team_user_from_open_id_service

--- app/services/slack/create_team_user_from_open_id_service.rb
+++ app/services/slack/create_team_user_from_open_id_service.rb
@@ -2,10 +2,11 @@
 
 module Slack
   class CreateTeamUserFromOpenIdService < ApplicationService
-    SIGN_IN_SLACK_REDIRECT_URI = "#{ENV.fetch('NGROK_URI', nil)}/api/v1/omniauth_callback/sign_in_slack".freeze
-    SIGN_IN_SLACK_URI = 'https://slack.com/openid/connect/authorize?response_type=code&client_id=' \
-                        "#{ENV.fetch('SLACK_CLIENT_ID', nil)}&scope=openid,profile,email&redirect_uri=" +
-                        SIGN_IN_SLACK_REDIRECT_URI.freeze
+    SIGN_IN_SLACK_REDIRECT_URI = Rails.application.routes.url_helpers.sign_in_slack_api_v1_omniauth_callback_url.fr
+
+    SIGN_IN_SLACK_URI = 'https://slack.com/openid/connect/authorize?response_type=code&scope=openid,profile,email&' \
+                        "redirect_uri=#{SIGN_IN_SLACK_REDIRECT_URI}&client_id=#{ENV.fetch('SLACK_CLIENT_ID', nil)}"
+                        .freeze
 
     def initialize(
       slack_client: Slack::Web::Client.new
@@ -14,9 +15,8 @@ module Slack
     end
 
     def call(code:)
-      access_token = get_openid_access_token(code)
-      user_info_response = get_user_info_response(access_token)
-      create_team_user!(access_token, user_info_response)
+      fetch_openid_access_token(code)
+      create_team_user!(fetch_openid_user_info)
 
       Success()
     rescue ActiveRecord::RecordInvalid => e
@@ -25,7 +25,7 @@ module Slack
 
     private
 
-    def get_openid_access_token(code)
+    def fetch_openid_access_token(code)
       openid_connect_token = @slack_client.openid_connect_token(
         {
           client_id: ENV.fetch('SLACK_CLIENT_ID', nil),
@@ -34,19 +34,19 @@ module Slack
           code:
         }
       )
-      openid_connect_token['access_token']
+      @access_token = openid_connect_token['access_token']
     end
 
-    def get_user_info_response(token)
-      @slack_client.openid_connect_userInfo(token:)
+    def fetch_openid_user_info
+      @slack_client.openid_connect_userInfo(token: @access_token)
     end
 
-    def create_team_user!(access_token, user_info_response)
-      TeamUser.create!(email: user_info_response['email'],
-                       token: access_token,
-                       name: user_info_response['name'],
+    def create_team_user!(user_info_response)
+      TeamUser.create!(id: user_info_response['https://slack.com/user_id'],
                        team_id: user_info_response['https://slack.com/team_id'],
-                       id: user_info_response['https://slack.com/user_id'])
+                       email: user_info_response['email'],
+                       name: user_info_response['name'],
+                       token: @access_token)
     end
   end
 end
